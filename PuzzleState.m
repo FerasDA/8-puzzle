ed3097f5dc5166ebdd56c9e468378dd9a9edb4c2
ferasda <FerasDA@users.noreply.github.com>
2015-02-19 17:18:08 -0500

    Puzzle state should be done

--- PuzzleState.m
+++ PuzzleState.m
@@ -37,6 +37,101 @@ classdef PuzzleState < handle
     	function s = getScore(obj)
         		s = obj.score;
     	end
+    end
+    
+    methods (Static = true)
+
+    	%finds the score of the state passed in
+    	function score = findScore(state)
+        	score = 0;
+        	for i = 1:9
+            	c= state(i);
+            	num = str2num(c);
+            	if (num ~= 0)
+               	xDif = (num-1)/3 - (i-1)/3;
+               	yN = mod(num,3);
+               	yI = mod(i,3);
+               	if(yN==0)
+                		yN=3;
+               	end
+               	if(yI ==0)
+                		yI=3;
+               	end
+               	yDif = yI-yN;
+               	dis = abs( xDif ) + abs( yDif );
+               	score = score + dis ;
+            	end           	 
+        	end
+    	end
+   	 
+    	%finds zero in state passed in
+    	function index = zeroIndex(state)
+        		index = strfind(state,'0');
+    	end
+   	 
+    	%test to see if it can move in state passed in
+    	function bool = canMoveUp(state)
+        		zerol = PuzzleState.zeroIndex(state);
+        		bool = (zerol>3);
+    	end
+   	 
+    	function bool = canMoveDown(state)
+        		zerol = PuzzleState.zeroIndex(state);
+        		bool = (zerol<7);
+    	end
+   	 
+    	function bool = canMoveLeft(state)
+        		zerol = PuzzleState.zeroIndex(state);
+        		bool = (mod(zerol,3) ~= 1);
+    	end
+   	 
+    	function bool = canMoveRight(state)
+        		zerol = PuzzleState.zeroIndex(state);
+        		bool = (mod(zerol,3) ~= 0);
+    	end
+   	 
+   	 
+    	%makes a new states by moving
+    	function state = moveUp(s)
+        	state = s;
+        	zerol = PuzzleState.zeroIndex(state);
+        	temp = s(zerol -3);
+        	state = strrep(state, temp, '9');
+        	state = strrep(state, '0', temp);
+        	state = strrep(state, '9', '0');
+    	end
+   	 
+     	function state = moveDown(s)
+        	state = s;
+        	zerol = PuzzleState.zeroIndex(state);
+        	temp = s(zerol +3);
+        	state = strrep(state, temp, '9');
+        	state = strrep(state, '0', temp);
+        	state = strrep(state, '9', '0');
+     	end
+   	 
+    	function state = moveRight(s)
+        	state = s;
+        	zerol = PuzzleState.zeroIndex(state);
+        	temp = s(zerol +1);
+        	state = strrep(state, temp, '9');
+        	state = strrep(state, '0', temp);
+        	state = strrep(state, '9', '0');
+    	end
+   	 
+    	function state = moveLeft(s)
+        	state = s;
+        	zerol = PuzzleState.zeroIndex(state);
+        	temp = s(zerol -1);
+        	state = strrep(state, temp, '9');
+        	state = strrep(state, '0', temp);
+        	state = strrep(state, '9', '0');
+    	end  	 
+   	 
+    	%sees if the state passed in is a new state
+    	function bool = newState(knownStates, state)
+        	bool = ~(ismember(state,knownStates));
+    	end
 	end
 end
 
